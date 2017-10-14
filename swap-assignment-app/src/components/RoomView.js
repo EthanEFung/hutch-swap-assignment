2442c98801de9ff685339becf0f62b8b06a31a62
Ethan Fung <EthanEFung@gmail.com>
2017-10-14 11:17:34 -0700

    Define Views of product swap
    Looking over the gif, the first thing to note is that there are
    two view components, one of the room and one for similar products.

---
+++ swap-assignment-app/src/components/RoomView.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class RoomView extends Component {
+  render() {
+    return <div>I am the room view</div>;
+  }
+}
+
+export default RoomView;
---
+++ swap-assignment-app/src/components/SimilarProductsView.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class SimilarProductsView extends Component {
+  render() {
+    return <div>I am the similar products view</div>;
+  }
+}
+
+export default SimilarProductsView;
