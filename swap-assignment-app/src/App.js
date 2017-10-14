41e794ff010c33d0479b4727d7adbb2d19942714
Ethan Fung <EthanEFung@gmail.com>
2017-10-14 11:20:08 -0700

    Define seperate view folders for modularity
    Each view will have a series of sub components.  Defined
    directories to promote greater organization

--- swap-assignment-app/src/App.js
+++ swap-assignment-app/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import RoomView from "./components/RoomView";
-import SimilarProductsView from "./components/SimilarProductsView";
+import RoomView from "./components/RoomView/RoomView";
+import SimilarProductsView from "./components/SimilarProductsView/SimilarProductsView";
 
 class App extends Component {
   render() {
