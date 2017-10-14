bf4a01ede5548957e87ba0d6f694705f5a187eee
Ethan Fung <EthanEFung@gmail.com>
2017-10-14 11:33:31 -0700

    Instantiate dummy data for exercise and promisify the function

--- swap-assignment-app/src/App.js
+++ swap-assignment-app/src/App.js
@@ -3,11 +3,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import RoomView from "./components/RoomView/RoomView";
 import SimilarProductsView from "./components/SimilarProductsView/SimilarProductsView";
+import getData from "./dummyData";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+  }
+
+  componentDidMount() {
+    getData.then(res => console.log(res));
   }
   render() {
     return (
